d8bb7dc8f5794e7f992bae0a33e08e246b1a4058
Aaron <aaronperezwindows10@gmail.com>
2023-12-01 17:42:42 -0600

    deleting api key

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD3TU82t_oL3p1ycFT6U5rpaBX4nLI0y_Y',
-  authDomain: 'proyecto-34d45.firebaseapp.com',
-  projectId: 'proyecto-34d45',
-  storageBucket: 'proyecto-34d45.appspot.com',
-  messagingSenderId: '833783779934',
-  appId: '1:833783779934:web:a80047f1e68dcd3912dacf'
+  
 }
 
 // Initialize Firebase
